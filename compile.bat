757ea682062c4db5bc5955da40f56911c7a27265
ssbmars <46971717+ssbmars@users.noreply.github.com>
2022-08-05 05:14:15 -0700

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -2,4 +2,8 @@
 armips build.asm -strequ GameNum 4 -strequ GameName bm
 armips build.asm -strequ GameNum 4 -strequ GameName rs
 textpet run-script compile.tpl
+
+flips -c -b "rom/exe4bm.gba" "out/ROCK_EXE4_BMB4BJ_00.gba" "out/ROCK_EXE4_BMB4BJ_00.bps"
+flips -c -b "rom/exe4rs.gba" "out/ROCK_EXE4_RSB4WJ_01.gba" "out/ROCK_EXE4_RSB4WJ_01.bps"
+
 timeout 3
