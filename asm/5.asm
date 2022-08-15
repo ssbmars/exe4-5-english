8e27cc3e806bf56b5782d2b780625ee034df3b49
ssbmars <46971717+ssbmars@users.noreply.github.com>
2022-08-15 03:27:07 -0700

    fix circlegun for exe5 too

--- asm/5.asm
+++ asm/5.asm
@@ -20,3 +20,34 @@
 // lowercase letters
 .org exe5charset - 0x4F70
 .import "rom\bn5tb.gba",0x6C2320,0x9A0
+
+// fix open bus reads that can stall the game by failing
+
+.org openbus0
+	mov r0,0
+.org openbus1
+	mov r0,0
+.org openbus2
+	mov r0,0
+.org openbus3
+	mov r0,0
+.org openbus4
+	mov r0,0
+.org openbus5
+	mov r0,0
+.org openbus6
+	mov r0,0
+.org openbus7
+	mov r0,0
+.org openbus8
+	mov r0,0
+.org openbus9
+	mov r0,0
+.org openbus10
+	mov r0,0
+.org openbus11
+	mov r0,0
+.org openbus12
+	mov r0,0
+.org openbus13
+	mov r0,0
--- asm/5tb_addr.asm
+++ asm/5tb_addr.asm
@@ -1,1 +1,16 @@
 .definelabel exe5charset, 0x086E23C4
+
+.definelabel openbus0, 0x8014154
+.definelabel openbus1, 0x8014212
+.definelabel openbus2, 0x80142f8
+.definelabel openbus3, 0x80c74a4
+.definelabel openbus4, 0x80c9800
+.definelabel openbus5, 0x80cfb70
+.definelabel openbus6, 0x80cfcda
+.definelabel openbus7, 0x80d4bb8
+.definelabel openbus8, 0x80d6298
+.definelabel openbus9, 0x80d718c
+.definelabel openbus10, 0x80d86cc
+.definelabel openbus11, 0x80d975c
+.definelabel openbus12, 0x80d9984
+.definelabel openbus13, 0x80d9b0a
